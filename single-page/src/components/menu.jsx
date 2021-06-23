5e331da1e7523d1a3306a878feebbedec2016d01
cristian-bars <patxi.cristian@gmail.com>
2021-06-23 12:56:23 +0200

    Editing menu mobile first

--- single-page/src/components/menu.jsx
+++ single-page/src/components/menu.jsx
@@ -1,21 +1,28 @@
 import React, { useState } from 'react';
 import {main} from '../assets/menu_data.json';
 import MenuMoreDetails from './menuMoreDetails';
+import './style.css';
 
 function Menu() {
   const [selectedMenu, setSelectedMenu] = useState(null);
-  console.log(main);
   const item = main.map((item) => (
-    <li key={item.key}>
-      {item.title}
-      <button type="button" onClick={() => setSelectedMenu(item)}>^</button>
-      {selectedMenu === item && <MenuMoreDetails item={item.below} />}
-    </li>
+      <div className="listItem">
+      <button type="button" onClick={() => setSelectedMenu(item)}>{item.title}</button>
+      {
+      item.below && (selectedMenu === item && <MenuMoreDetails item={item.below} />)
+      }
+      </div>
   ));
   return (
-    <ul className="list-container">
-      {item}
-    </ul>
+      <div className="container">
+          <div className="header">
+              <img src="https://www.culinaryartsswitzerland.com/bundles/segshared/img/caa/logo-caa.png" alt="culinaryartsswitzerland"/>
+          </div>
+        <div className="menuList">
+            {item}
+        </div>
+      </div>
+    
   );
 }
 
--- single-page/src/components/menuMoreDetails.jsx
+++ single-page/src/components/menuMoreDetails.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const MenuMoreDetails = ({ item }) => (
     item? (
         item.map((detail) => (
-        <li>{detail.title}</li>
+        <div className="itemDetail">{detail.title}</div>
     ))) : (
         <div>Ni hi ha items</div>
     ))
